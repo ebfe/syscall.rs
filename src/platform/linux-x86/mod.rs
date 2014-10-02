a52958523bbe6aa98eefe8bdf35d0befdfe8ff4d
Michael Gehring <mg@ebfe.org>
2014-10-02 16:31:03 +0200

    fallback to int 0x80

--- src/platform/linux-x86/mod.rs
+++ src/platform/linux-x86/mod.rs
@@ -42,44 +42,65 @@ pub unsafe fn setup_vsyscall(auxv: *const u8) -> bool {
 #[inline(always)]
 pub unsafe fn syscall0(n: uint) -> uint {
     let mut ret : uint;
-    asm!("call *vsyscall"
-                      : "={eax}"(ret)
-                      : "{eax}"(n)
-                      : "memory" "cc"
-                      : "volatile");
+    asm!("    cmpl $$0, vsyscall
+              jz 1f
+              call *vsyscall
+              jmp 2f
+          1:  int $$0x80
+          2:"
+          : "={eax}"(ret)
+          : "{eax}"(n)
+          : "memory" "cc"
+          : "volatile");
     ret
 }
 
 #[inline(always)]
 pub unsafe fn syscall1(n: uint, a1: uint) -> uint {
     let mut ret : uint;
-    asm!("call *vsyscall"
-                      : "={eax}"(ret)
-                      : "{eax}"(n), "{ebx}"(a1)
-                      : "memory" "cc"
-                      : "volatile");
+    asm!("    cmpl $$0, vsyscall
+              jz 1f
+              call *vsyscall
+              jmp 2f
+          1:  int $$0x80
+          2:"
+          : "={eax}"(ret)
+          : "{eax}"(n), "{ebx}"(a1)
+          : "memory" "cc"
+          : "volatile");
     ret
 }
 
 #[inline(always)]
 pub unsafe fn syscall2(n: uint, a1: uint, a2: uint) -> uint {
     let mut ret : uint;
-    asm!("call *vsyscall"
-                      : "={eax}"(ret)
-                      : "{eax}"(n), "{ebx}"(a1), "{ecx}"(a2)
-                      : "memory" "cc"
-                      : "volatile");
+    asm!("    cmpl $$0, vsyscall
+              jz 1f
+              call *vsyscall
+              jmp 2f
+          1:  int $$0x80
+          2:"
+          : "={eax}"(ret)
+          : "{eax}"(n), "{ebx}"(a1), "{ecx}"(a2)
+          : "memory" "cc"
+          : "volatile");
     ret
 }
 
 #[inline(always)]
 pub unsafe fn syscall3(n: uint, a1: uint, a2: uint, a3: uint) -> uint {
     let mut ret : uint;
-    asm!("call *vsyscall"
-                      : "={eax}"(ret)
-                      : "{eax}"(n), "{ebx}"(a1), "{ecx}"(a2), "{edx}"(a3)
-                      : "memory" "cc"
-                      : "volatile");
+
+    asm!("    cmpl $$0, vsyscall
+              jz 1f
+              call *vsyscall
+              jmp 2f
+          1:  int $$0x80
+          2:"
+          : "={eax}"(ret)
+          : "{eax}"(n), "{ebx}"(a1), "{ecx}"(a2), "{edx}"(a3)
+          : "memory" "cc"
+          : "volatile");
     ret
 }
 
@@ -87,12 +108,17 @@ pub unsafe fn syscall3(n: uint, a1: uint, a2: uint, a3: uint) -> uint {
 pub unsafe fn syscall4(n: uint, a1: uint, a2: uint, a3: uint,
                                 a4: uint) -> uint {
     let mut ret : uint;
-    asm!("call *vsyscall" 
-                      : "={eax}"(ret)
-                      : "{eax}"(n), "{ebx}"(a1), "{ecx}"(a2), "{edx}"(a3),
-                        "{esi}"(a4)
-                      : "memory" "cc"
-                      : "volatile");
+    asm!("    cmpl $$0, vsyscall
+              jz 1f
+              call *vsyscall
+              jmp 2f
+          1:  int $$0x80
+          2:"
+          : "={eax}"(ret)
+          : "{eax}"(n), "{ebx}"(a1), "{ecx}"(a2), "{edx}"(a3),
+            "{esi}"(a4)
+          : "memory" "cc"
+          : "volatile");
     ret
 }
 
@@ -100,12 +126,17 @@ pub unsafe fn syscall4(n: uint, a1: uint, a2: uint, a3: uint,
 pub unsafe fn syscall5(n: uint, a1: uint, a2: uint, a3: uint,
                                 a4: uint, a5: uint) -> uint {
     let mut ret : uint;
-    asm!("call *vsyscall"
-                      : "={eax}"(ret)
-                      : "{eax}"(n), "{ebx}"(a1), "{ecx}"(a2), "{edx}"(a3),
-                        "{esi}"(a4), "{edi}"(a5)
-                      : "memory" "cc"
-                      : "volatile");
+    asm!("    cmpl $$0, vsyscall
+              jz 1f
+              call *vsyscall
+              jmp 2f
+          1:  int $$0x80
+          2:"
+          : "={eax}"(ret)
+          : "{eax}"(n), "{ebx}"(a1), "{ecx}"(a2), "{edx}"(a3),
+            "{esi}"(a4), "{edi}"(a5)
+          : "memory" "cc"
+          : "volatile");
     ret
 }
 
@@ -144,16 +175,20 @@ pub unsafe fn syscall6(n: usize, a1: usize, a2: usize, a3: usize,
     // restore ebp.
     let args = [n, a1, a2, a3, a4, a5, a6];
 
-    asm!("push %ebp
-          movl 24(%eax), %ebp
-          movl 20(%eax), %edi
-          movl 16(%eax), %esi
-          movl 12(%eax), %edx
-          movl  8(%eax), %ecx
-          movl  4(%eax), %ebx
-          movl  0(%eax), %eax
-          call *vsyscall
-          pop %ebp"
+    asm!("    push %ebp
+              movl 24(%eax), %ebp
+              movl 20(%eax), %edi
+              movl 16(%eax), %esi
+              movl 12(%eax), %edx
+              movl  8(%eax), %ecx
+              movl  4(%eax), %ebx
+              movl  0(%eax), %eax
+              cmpl $$0, vsyscall
+              jz 1f
+              call *vsyscall
+              jmp 2f
+          1:  int $$0x80
+          2:  pop %ebp"
             : "={eax}"(ret)
             : "{eax}"(args)
             : "ebx" "ecx" "edx" "esi" "edi" "ebp" "memory" "cc"
